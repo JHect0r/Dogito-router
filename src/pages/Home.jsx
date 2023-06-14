f148257c57547c754713aeac4b1d2ed66b1c18e5
JHect0r <ach4242_24@hotmail.com>
2023-06-13 19:03:42 -0600

    Clase04 Exhibiendo post y categoría

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import ListPost from "../components/ListPosts";
+import ListCategories from "../components/ListCategories";
 const Home = () => {
   return (
     <main>
       <div className="container">
         <h2 className="title-page">Pet noticias</h2>
       </div>
+      <ListCategories />
       <ListPost url={"/posts"} />
     </main>
   );
